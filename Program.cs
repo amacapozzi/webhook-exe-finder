7e1782ae0d577feceed46e08b8600ecf55c5d404
amacapozzi <amacapozzi07@gmail.com>
2024-06-16 10:05:55 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+public class ExeLinkParser
+{
+    private static readonly string url = @"https://discord\.com/api/webhooks/\d+/[A-Za-z0-9_-]+";
+
+    private static HttpClient httpClient = new HttpClient();
+
+    private static void Main(string[] args)
+    {
+        try
+        {
+            string filePath = "C:\\Users\\Student\\Downloads\\ProcessWatcher-master\\ProcessWatcher-master\\ProcessWatcher\\bin\\Debug\\ProcessWatcher.exe";
+            string foundWebhook = FindWebhookUrl(filePath);
+
+            while (foundWebhook != null)
+            {
+                HttpResponseMessage responseMessage = FetchDiscordUrl(foundWebhook, "sexo").GetAwaiter().GetResult();
+
+                if (!responseMessage.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Error sending message");
+                }
+
+                Console.WriteLine("Message sended");
+            }
+        }
+        catch
+        {
+        }
+
+        Console.ReadKey();
+    }
+
+    public static async Task<HttpResponseMessage> FetchDiscordUrl(string webhookUrl, string message)
+    {
+        var payload = new
+        {
+            content = message
+        };
+
+        var json = JsonConvert.SerializeObject(payload);
+
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        return await httpClient.PostAsync(webhookUrl, content); ;
+    }
+
+    public static string FindWebhookUrl(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException("File not found.");
+        }
+
+        byte[] fileBytes = File.ReadAllBytes(filePath);
+        string fileContent = Encoding.ASCII.GetString(fileBytes);
+
+        StringBuilder filteredContent = new StringBuilder();
+        foreach (char c in fileContent)
+        {
+            if (c >= 32 && c <= 126)
+            {
+                filteredContent.Append(c);
+            }
+        }
+
+        Regex regex = new Regex(url, RegexOptions.IgnoreCase);
+        Match match = regex.Match(filteredContent.ToString());
+
+        return match.Success ? match.Value : null;
+    }
+}
